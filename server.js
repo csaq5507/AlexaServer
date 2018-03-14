854e0efb197c2e0990dea48f78e0b7e6ca8867f9
Ivan Waldboth <Ivan.Waldboth@student.uibk.ac.at>
2018-03-14 14:16:26 +0100

    wl

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
-var AlexaAppServer = require( 'alexa-app-server');
+
+var AlexaAppServer = require( 'alexa-app-server' );
+
 var server = new AlexaAppServer( {
-	httpsEnabled : false,
+	httpsEnabled: false,
 	port: process.env.PORT || 80
 } );
 
